23e2705c6de5b9fc64c455fce271e1ef6d067605
lizhongqiang <328656732@qq.com>
2017-02-16 17:37:37 +0800

    可播放视频，待优化

---
+++ DQPlayer/DQPlayer/DQVideoPlayerView.h
@@ -0,0 +1,39 @@
+//
+//  DQVideoPlayerView.h
+//  DQPlayer
+//
+//  Created by  lizhongqiang on 2017/2/16.
+//  Copyright © 2017年  lizhongqiang. All rights reserved.
+//
+//  psylife
+//  @class DQVideoPlayerView
+//  @description 视频封装层
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@class DQVideoPlayerView;
+@protocol DQVideoPlayerViewDelegate <NSObject>
+
+//全屏/非全屏切换
+- (void)videoPlayer:(DQVideoPlayerView *)videoPlayer clickFullButton:(UIButton *)fullButton;
+
+@end
+@interface DQVideoPlayerView : UIView
+@property (nonatomic, strong) AVPlayerLayer *playerLayer;
+@property (nonatomic, strong) NSString *playUrl;
+@property (nonatomic, weak) id<DQVideoPlayerViewDelegate> delegate;
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, strong) UIView *topView;
+@property (nonatomic, strong) UIView *bottomView;
+@property (nonatomic, strong) UIButton *pauseAndPlayBtn;
+@property (nonatomic, strong) UIButton *fullBtn;
+
+
+
+- (void)play;
+
+
+
+@end
